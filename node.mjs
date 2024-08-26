ef0c284eff0878a674cb3b2615d915ef901332d7
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-08-25 23:50:44 -0400

    Add constructor and findMoves() method to node

---
+++ node.mjs
@@ -0,0 +1,25 @@
+export default class Node {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+    this.moves = findMoves(this.row, this.col);
+  }
+
+  findMoves(row, col) {
+    let legalMoves = [];
+    let allMoves = [
+      [row + 2, col + 1],
+      [row + 2, col - 1],
+      [row - 2, col + 1],
+      [row - 2, col - 1],
+      [row + 1, col + 2],
+      [row + 1, col - 2],
+      [row - 1, col + 2],
+      [row - 1, col - 2],
+    ];
+    legalMoves = allMoves.filter((move) => {
+      move[0] < 8 && move[0] >= 0 && move[1] < 8 && move[1] >= 0;
+    });
+    return legalMoves;
+  }
+}
